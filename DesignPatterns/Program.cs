f8714d65152c1fa228c1bfc76027fe77eb819dd6
Evandro Luis <evandrolrff@outlool.com>
2019-08-19 11:39:26 -0300

    First commit

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Padroes de projeto");
+            var menu = new Menu();
+            menu.SubMenu();
+        }
+        
+    }
+}

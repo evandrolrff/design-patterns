32193721efb09f38195ab1712d41e2d52aeb1297
Evandro Luis <evandrolrff@outlool.com>
2020-01-20 20:01:52 -0300

    add proxy pattern

---
+++ DesignPatterns/Proxy/Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Proxy {
+    class Client {
+        public Client() {
+            Program program = new Program();
+
+            Console.WriteLine("Program: Executing the client code with a real subject:");
+            RealSubject realSubject = new RealSubject();
+            program.ProgramCode(realSubject);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Program: Executing the same client code with a proxy:");
+            Proxy proxy = new Proxy(realSubject);
+            program.ProgramCode(proxy);
+        }
+    }
+}

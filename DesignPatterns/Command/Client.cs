617ede837f5fd582794be9bb4951d1d58d3f1a06
Evandro Luis <evandrolrff@outlool.com>
2020-01-20 20:01:34 -0300

    add command pattern

---
+++ DesignPatterns/Command/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Command {
+    class Client {
+        public Client() {
+            // The client code can parameterize an invoker with any commands.
+            Invoker invoker = new Invoker();
+            invoker.SetOnStart(new SimpleCommand("Say Hi!"));
+            Receiver receiver = new Receiver();
+            invoker.SetOnFinish(new ComplexCommand(receiver, "Send email", "Save report"));
+
+            invoker.DoSomethingImportant();
+        }
+    }
+}
---
+++ DesignPatterns/Command/ICommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Command {
+    // The Command interface declares a method for executing a command.
+    interface ICommand {
+        void Execute();
+    }
+}

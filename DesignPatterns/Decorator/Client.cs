04b558cb1176bc6ef66cff4ffe1b9362f785c3f8
Evandro Luis <evandrolrff@outlool.com>
2020-01-19 21:13:38 -0300

    Add Decorator pattern

---
+++ DesignPatterns/Decorator/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Decorator {
+    class Client {
+        public Client() {
+            Program program = new Program();
+
+            var simple = new ConcreteComponent();
+            Console.WriteLine("Program: I get a simple component:");
+            program.ProgramCode(simple);
+            Console.WriteLine();
+
+            // ...as well as decorated ones.
+            //
+            // Note how decorators can wrap not only simple components but the
+            // other decorators as well.
+            ConcreteDecoratorA decorator1 = new ConcreteDecoratorA(simple);
+            ConcreteDecoratorB decorator2 = new ConcreteDecoratorB(decorator1);
+            Console.WriteLine("Program: Now I've got a decorated component:");
+            program.ProgramCode(decorator2);
+        }
+    }
+}
---
+++ DesignPatterns/Decorator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Decorator {
+    class Program {
+        // The Program code works with all objects using the Component interface.
+        // This way it can stay independent of the concrete classes of
+        // components it works with.
+        public void ProgramCode(Component component) {
+            Console.WriteLine("RESULT: " + component.Operation());
+        }
+    }
+}

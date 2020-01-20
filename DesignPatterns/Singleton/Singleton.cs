7bc91c694cca97b6193bd4ad9ab6ecd5cb2855b7
Evandro Luis <evandrolrff@outlool.com>
2020-01-19 21:14:18 -0300

    add singleton pattern

---
+++ DesignPatterns/Singleton/Singleton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Singleton {
+    class Singleton {
+
+        private Singleton() { }
+
+        private static Singleton _instance;
+
+        public static Singleton GetInstance() {
+            if (_instance == null) {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+
+        public static void someBusinessLogic() {
+            // ...
+        }
+    }
+}
